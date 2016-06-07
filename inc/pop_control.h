49bb2e874d6aeed05ec93eac4c3702ad464b910b
Hao Shi <boruoshihao@gmail.com>
2016-06-07 17:05:57 -0400

    Start the library for population control.

---
+++ inc/pop_control.h
@@ -0,0 +1,9 @@
+#ifndef POP_CONTROL_H
+#define POP_CONTROL_H
+
+#include<iostream>
+#include<vector>
+
+std::vector<long> pop_configuration(const std::vector<long>& weight);
+
+#endif
---
+++ src/pop_control.cpp
@@ -0,0 +1,9 @@
+#include "pop_control.h"
+
+using namespace std;
+
+vector<long> pop_configuration(const vector<long>& weight)
+{
+    vector<long> table;
+    return table;
+}
