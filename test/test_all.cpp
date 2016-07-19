8810bbdc35e3a4d6ba6d09a7dcd06d5f6b2a4af1
Hao Shi <boruoshihao@gmail.com>
2016-07-19 16:15:50 -0400

    use mpi_hao in test_all.cpp

--- test/test_all.cpp
+++ test/test_all.cpp
@@ -1,7 +1,5 @@
-#ifdef MPI_HAO
-#include <mpi.h>
-#endif
 #include <iostream>
+#include "mpi_fun.h"
 #include "random_hao.h"
 
 using namespace std;
@@ -10,19 +8,12 @@ void pop_control_test();
 
 int main(int argc, char** argv)
 {
-    int rank=0;
+    MPIInitFunnel(argc,argv);
 
-#ifdef MPI_HAO
-    MPI_Init(&argc,&argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-#endif
-
-    if(rank==0) cout<<"\n\n\n=======Testing======="<<endl;
+    if( MPIRank()==0 ) cout<<"\n\n\n=======Testing======="<<endl;
     pop_control_test();
 
-#ifdef MPI_HAO
-    MPI_Finalize();
-#endif
+    MPIFinalize();
 
     return 0;
 }
